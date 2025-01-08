e20cb803e55ca5ba046cebbb5a65cc9305ce35c9
noyli1 <noyli@mta.ac.il>
2025-01-08 12:24:18 +0200

    Update docusign_api.py
    Noy putting note

--- routes/docusign_api.py
+++ routes/docusign_api.py
@@ -1,3 +1,4 @@
+# Import necessary modules and libraries.
 import ssl
 import base64
 import requests
@@ -7,12 +8,14 @@ from retrying import retry
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from the .env file.
 load_dotenv()
 
+# Retrieve DocuSign account ID and access token from environment variables.
 DOCUSIGN_ACCOUNT_ID = os.getenv('DOCUSIGN_ACCOUNT_ID')
 DOCUSIGN_ACCESS_TOKEN = os.getenv('DOCUSIGN_ACCESS_TOKEN')
 
-
+# Function to encode a document in Base64 format for API usage.
 def encode_document(file_path):
     with open(file_path, "rb") as file:
         file_data = file.read()
@@ -20,11 +23,11 @@ def encode_document(file_path):
     encoded = base64.b64encode(file_data).decode("utf-8")
     return encoded
 
-
-# Retry logic for transient errors
+# Retry logic decorator: Retries up to 3 times with a 5-second interval between attempts
 @retry(stop_max_attempt_number=3, wait_fixed=5000)
 def send_signature_request(file_path, signer_email, signer_name, document_id):
     print(file_path, signer_name, signer_email)
+# Payload for the DocuSign API request.
     payload = {
         "documents": [
             {
@@ -46,16 +49,16 @@ def send_signature_request(file_path, signer_email, signer_name, document_id):
         },
         "status": "sent"
     }
-
+# Headers for the API request, including the authorization token.
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQsAAAABAAUABwCA-DSVwyzdSAgAgDhYowYt3UgCAHdIv1qqLk5BoFaxEE7Df3gVAAEAAAAYAAIAAAAFAAAAHQAAAA0AJAAAADY0MTczZTJiLTk4NTMtNDY0ZC1iOWZjLTE5ZGNiZDVkODMwMyIAJAAAADY0MTczZTJiLTk4NTMtNDY0ZC1iOWZjLTE5ZGNiZDVkODMwMxIAAQAAAAsAAABpbnRlcmFjdGl2ZTAAgMsDlMMs3Ug3AOGTXiaUgndHnjZtqG2VTfk.f0Zgohfk_gyG0DDXsy9kxlqOYlwikh1xqZPbVe2CG9k-GEsBJkcF1QNsjTmzFDr9c6G_k603cj20OiQCcTBQWWv_wyNDN4tmRx9L9tiQzqj9HfyMcFSIlnjczH77gBoEZX-Lgy94ZDyY_4HoGakDy-KKD9NhQyRKUTDn_-5dPbcGoOtOLgzmeryLRYab1mpbeqBNTA-sVPeo9goOl-9dT80NCoplc_4URKUYdsSLHkuAVZdmVfqDf8br5mf-h6VKMZZH5IVuwYQNofL61xWAv1_oDRz8wo9U_bMKGioBagFBopNbwYqzqtRz0pzc7zu2qTJRkeil5TwvJa-p4tDGtg'
-        # Replace with your actual DocuSign access token
     }
 
     try:
+# Create a new HTTP session for making requests.
         session = requests.Session()
-
+# Send a POST request to the DocuSign API to create an envelope.
         response = session.post(
             F"https://demo.docusign.net/restapi/v2.1/accounts/{os.getenv('DOCUSIGN_ACCOUNT_ID')}/envelopes",
             # Replace with your account ID
@@ -64,7 +67,7 @@ def send_signature_request(file_path, signer_email, signer_name, document_id):
             timeout=120
         )
 
-        # Check the response status
+# Check the response status.
         if response.status_code == 201:
             response_data = response.json()
             print(response_data)
@@ -75,7 +78,7 @@ def send_signature_request(file_path, signer_email, signer_name, document_id):
         else:
             print(f"Error: {response.status_code}, {response.text}")
             raise Exception("Error sending DocuSign request")
-
+# Handle exceptions related to the request.
     except requests.exceptions.RequestException as e:
         print(f"Error sending DocuSign request: {e}")
         raise
