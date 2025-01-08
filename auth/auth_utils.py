e568c2307041cea8a01fa0966c9b8cf57725faff
lihibn <Lihibn@mta.ac.il>
2025-01-08 12:01:32 +0200

    Update auth_utils.py
    Add comments

--- auth/auth_utils.py
+++ auth/auth_utils.py
@@ -1,3 +1,4 @@
+# Import necessary modules and libraries.
 from flask import Blueprint, request, jsonify, session, make_response
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from database.db import SessionLocal
@@ -5,22 +6,23 @@ from models.models import User,Property,Meeting
 import jwt
 from dotenv import load_dotenv
 import os
+
+# Load environment variables from the .env file.
 load_dotenv()
 
-
+# Verify JWT token sent in cookies.
 def verify_token():
-    """Verify JWT token sent in cookies."""
     current_user = get_jwt_identity()  # Get the current user from the token
     return jsonify({"status": "success", "user_id": current_user['user_id'], "role": current_user['role']}), 200
 
+# Utility function to decode JWT and fetch user details.
 def get_user_from_token(auth_token):
-    """Utility function to decode JWT and fetch user details."""
     session = SessionLocal()
     try:
         decoded_token = jwt.decode(auth_token, os.getenv('SECRET_KEY'), algorithms=["HS256"])
-        user_email = decoded_token.get('email')
-        user_role = decoded_token.get('role')
-        user = session.query(User).filter(User.email == user_email).first()
+        user_email = decoded_token.get('email') # Extract the email.
+        user_role = decoded_token.get('role') # Extract the role.
+        user = session.query(User).filter(User.email == user_email).first() # Query the database to find the user by email.
         return user, user_role
     except jwt.ExpiredSignatureError:
         return jsonify({"message": "Token has expired. Please sign in again."}), 401
